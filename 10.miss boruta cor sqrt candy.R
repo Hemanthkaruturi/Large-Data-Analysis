ae385b757ec9dcd90ed097c7f75d14678b1e286f
Hemanth <karuturihemanth@gmail.com>
2018-07-31 10:59:28 +0530

    Updated Lasso, GBM, XGBoost

--- 10.miss boruta cor sqrt candy.R
+++ 10.miss boruta cor sqrt candy.R
@@ -110,3 +110,103 @@ mape <- mean(abs((actuals_preds$predicteds - actuals_preds$actuals))/actuals_pre
 # summarize accuracy
 rmse <- sqrt(mean((data.miss.boruta.cor.sqrt.test$y - rf_pred)^2))
 #35.86611
+
+###################################### Lasso Model ############################################
+library(glmnet)
+library(Metrics)
+set.seed(123)
+lasso_regressor = cv.glmnet(as.matrix(data.miss.boruta.cor.sqrt.train[, -87]), data.miss.boruta.cor.sqrt.train$y)
+
+## Predictions
+lasso_pred <- predict(lasso_regressor, newx = as.matrix(data.miss.boruta.cor.sqrt.test[, -87]), s = "lambda.min")
+
+#Finding accuracy of the model using correlation
+actuals_preds <- data.frame(cbind(actuals=data.miss.boruta.cor.sqrt.test$y, predicteds=lasso_pred)) 
+correlation_accuracy <- cor(actuals_preds)
+print(correlation_accuracy) #0.9235009 highly positive correlation
+
+#minimum maximum accuracy
+# MinMaxAccuracy=mean(min(actuals,predicteds)/max(actuals,predicteds))
+# MeanAbsolutePercentageError (MAPE)=mean(abs(predicteds−actuals)/actuals)
+min_max_accuracy <- mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max))  
+#0.9437862
+mape <- mean(abs((lasso_pred - actuals_preds$actuals))/actuals_preds$actuals) 
+# 0.05968235
+
+
+#Calculate RMSE 
+
+rmse <- sqrt(mean((data.miss.boruta.test$y - lasso_pred)^2))
+#44.63455
+
+######################################### GBM ################################################
+library(iterators)
+library(parallel)
+library(doMC)
+library(caret)
+set.seed(222)
+## detectCores() returns 16 cpus
+registerDoMC(16)
+## Set up caret model training parameters
+CARET.TRAIN.CTRL <- trainControl(method = "repeatedcv", number = 5, repeats = 5, 
+                                 verboseIter = FALSE, allowParallel = TRUE)
+gbmFit <- train(y ~ ., method = "gbm", metric = "RMSE", maximize = FALSE, 
+                trControl = CARET.TRAIN.CTRL, tuneGrid = expand.grid(n.trees = (4:10) * 
+                                                                       50, interaction.depth = c(5), shrinkage = c(0.05), n.minobsinnode = c(10)), 
+                data = data.miss.boruta.cor.sqrt.train, verbose = FALSE)
+
+## print(gbmFit)
+
+## Predictions
+preds1 <- predict(gbmFit, newdata = data.miss.boruta.cor.sqrt.test)
+
+#Finding accuracy of the model using correlation
+actuals_preds <- data.frame(cbind(actuals=data.miss.boruta.cor.sqrt.test$y, predicteds=preds1)) 
+correlation_accuracy <- cor(actuals_preds)
+print(correlation_accuracy) # 0.9548024 highly positive correlation
+
+#minimum maximum accuracy
+# MinMaxAccuracy=mean(min(actuals,predicteds)/max(actuals,predicteds))
+# MeanAbsolutePercentageError (MAPE)=mean(abs(predicteds−actuals)/actuals)
+min_max_accuracy <- mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max))  
+#0.9579674
+mape <- mean(abs((preds1 - actuals_preds$actuals))/actuals_preds$actuals) 
+# 0.04394755
+
+
+#Calculate RMSE 
+
+rmse <- sqrt(mean((data.miss.boruta.cor.sqrt.test$y - preds1)^2))
+#34.58712
+
+#################################### XGBOOST #################################################
+library(xgboost)
+set.seed(123)
+## Model parameters trained using xgb.cv function
+xgbFit = xgboost(data = as.matrix(data.miss.boruta.cor.sqrt.train[, -87]), nfold = 5, label = as.matrix(data.miss.boruta.cor.sqrt.train$y), 
+                 nrounds = 2200, verbose = FALSE, objective = "reg:linear", eval_metric = "rmse", 
+                 nthread = 8, eta = 0.01, gamma = 0.0468, max_depth = 6, min_child_weight = 1.7817, 
+                 subsample = 0.5213, colsample_bytree = 0.4603)
+## print(xgbFit)
+
+## Predictions
+xgb_pred <- predict(xgbFit, newdata = as.matrix(data.miss.boruta.cor.sqrt.test[,c(-87)]))
+
+#Finding accuracy of the model using correlation
+actuals_preds <- data.frame(cbind(actuals=data.miss.boruta.cor.sqrt.test$y, predicteds=xgb_pred)) 
+correlation_accuracy <- cor(actuals_preds)
+print(correlation_accuracy) #0.960546 highly positive correlation
+
+#minimum maximum accuracy
+# MinMaxAccuracy=mean(min(actuals,predicteds)/max(actuals,predicteds))
+# MeanAbsolutePercentageError (MAPE)=mean(abs(predicteds−actuals)/actuals)
+min_max_accuracy <- mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max))  
+#0.9608608
+mape <- mean(abs((xgb_pred - actuals_preds$actuals))/actuals_preds$actuals) 
+# 0.04091538
+
+
+#Calculate RMSE 
+
+rmse <- sqrt(mean((data.miss.boruta.test$y - xgb_pred)^2))
+#32.33968
